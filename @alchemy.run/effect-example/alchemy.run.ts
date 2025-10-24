47aef066d4c6b40a4901d588463ca9d47a2fd5aa
Sam Goodwin <sam@alchemy.run>
2025-10-24 00:18:24 -0700

    chore: update example to just reference the service classes

--- @alchemy.run/effect-example/alchemy.run.ts
+++ @alchemy.run/effect-example/alchemy.run.ts
@@ -1,32 +1,15 @@
 import * as Alchemy from "@alchemy.run/effect";
 import * as AWS from "@alchemy.run/effect-aws";
-import * as Lambda from "@alchemy.run/effect-aws/lambda";
 import * as AlchemyCLI from "@alchemy.run/effect-cli";
 import { FetchHttpClient } from "@effect/platform";
 import { NodeContext } from "@effect/platform-node";
 import * as Effect from "effect/Effect";
-import { join } from "node:path";
 import { MyMonitor } from "./src/component.ts";
 import { Api, Consumer } from "./src/index.ts";
 
-const src = join(import.meta.dirname, "src");
-
-const api = Lambda.Function(Api, {
-  main: join(src, "api.ts"),
-  url: true,
-});
-
-const consumer = Lambda.Function(Consumer, {
-  main: join(src, "consumer.ts"),
-});
-
-const monitor = Lambda.Function(MyMonitor, {
-  main: join(src, "component.ts"),
-});
-
 const plan = Alchemy.plan({
   phase: process.argv.includes("--destroy") ? "destroy" : "update",
-  resources: [api, consumer, monitor],
+  resources: [Api, Consumer, MyMonitor],
 });
 
 const stack = await plan.pipe(
@@ -39,6 +22,6 @@ const stack = await plan.pipe(
   Effect.provide(Alchemy.app({ name: "my-iae-app", stage: "dev" })),
   Effect.provide(NodeContext.layer),
   Effect.provide(FetchHttpClient.layer),
-  Effect.tap((stack) => Effect.log(stack?.api.functionUrl)),
+  Effect.tap((stack) => Effect.log(stack?.Api.functionUrl)),
   Effect.runPromise,
 );
